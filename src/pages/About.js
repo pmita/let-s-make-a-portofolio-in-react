2800d5a268bbadb4cc0e487d931510d937ffd4e2
panos.mita2@outlook.com <pmita@dxc.com>
2021-05-06 10:48:07 +0100

    first page component created

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+//Let's import the About section component
+import AboutSection from '../components/AboutSection';
+
+const AboutUs = () => {
+    return (
+        <AboutSection />
+    );
+}
+
+export default AboutUs;
