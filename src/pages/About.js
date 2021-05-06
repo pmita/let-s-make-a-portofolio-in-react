b4703c4a42e85ecfd6761c02bc11c2db6c0fbab1
panos.mita2@outlook.com <pmita@dxc.com>
2021-05-06 10:59:15 +0100

    second component added

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,14 @@
 import React from 'react';
 //Let's import the About section component
 import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
 
 const AboutUs = () => {
     return (
-        <AboutSection />
+        <>
+            <AboutSection />
+            <ServicesSection />
+        </>
     );
 }
 
